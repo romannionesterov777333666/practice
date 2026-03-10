baf0bcb13574034c90fb1cfebf79e90382441749
NesterovRE <roma.nesterov777@gmail.com>
2026-03-10 14:36:02 +0700

    zz

--- main/build.gradle.kts
+++ main/build.gradle.kts
@@ -38,6 +38,12 @@ android {
 dependencies {
 
     implementation("com.android.support:appcompat-v7:28.0.0")
+    implementation("androidx.appcompat:appcompat:1.7.1")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.1")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.10.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.10.0")
+    implementation("com.google.android.material:material:1.4.0")
+    implementation("androidx.fragment:fragment-ktx:1.8.9")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("com.android.support.test:runner:1.0.2")
     androidTestImplementation("com.android.support.test.espresso:espresso-core:3.0.2")
